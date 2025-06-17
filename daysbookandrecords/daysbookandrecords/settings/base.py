a3db0d2e4a6a9ba6b5ca01de72003939d7bde92d
Jay Pandey <jay@Jays-MacBook-Air.local>
2025-06-17 10:33:28 +0530

    added dotenv

--- daysbookandrecords/daysbookandrecords/settings/base.py
+++ daysbookandrecords/daysbookandrecords/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -89,11 +92,11 @@ WSGI_APPLICATION = "daysbookandrecords.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "daysbooks",
-        "USER": "root",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": 3306
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT')
     }
 }
 
